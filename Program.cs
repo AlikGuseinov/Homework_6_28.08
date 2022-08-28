f6ebdd40db087e1839fb422f45f6db0154548261
Alik <9774952689@bk.ru>
2022-08-29 00:01:52 +0300

    Задача №6

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+    Console.WriteLine("Чисто четное");
+else
+    Console.WriteLine("Чисто нечетное");
